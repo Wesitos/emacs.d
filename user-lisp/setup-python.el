32e37e638617f5e42ac95bc1bc3d94e34401395a
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-02-12 05:24:23 -0500

    Modificaciones menores a la confg de python

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -1,9 +1,27 @@
 ;;Para utilizar Jedi (o cualquier de las alternativas de
-;; autocompletado) es necesario instalar un ambiente virtual
-;; de python (virtualnv) en .python-environments y ejecutar
-;; M-x jedi:install-server
+;; autocompletado) es necesario tener instalado el paquete
+;; virtualenv de python y ejecutar M-x jedi:install-server
 ;; jedi mostrara documentacion de los modulos instalados en el
-;; ambiente virtual .python-enviroments
+;; ambiente virtual .python-enviroments/jedi y en el scope
+;; global por defecto
+(require 'python)
+(require 'jedi)
+
+;; From https://github.com/jhamrick/emacs
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args (if system-is-mac
+                                   "--matplotlib=osx --colors=Linux"
+                                 (if system-is-linux
+                                     "--gui=gtk3 --matplotlib=gtk3 --colors=Linux"))
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 (add-hook 'python-mode-hook (lambda ()
                               (jedi:setup)
@@ -12,7 +30,6 @@
                               ))
 (setq jedi:complete-on-dot t)
 (setq jedi:environment-root
-      (expand-file-name ".python-environments" user-emacs-directory))
+      (expand-file-name "jedi" python-environment-directory))
 
 (provide 'setup-python)
-
