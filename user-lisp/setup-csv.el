651a5e84239e44623bf1dcb9587f70c0a945a683
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-05-02 17:55:25 -0500

    Agregado setup-csv

---
+++ user-lisp/setup-csv.el
@@ -0,0 +1,7 @@
+(setq csv-separators '("," " " "	"))
+
+(add-hook 'csv-mode-hook (lambda ()
+                           (setq tab-width 16)))
+(define-key csv-mode-map (kbd "TAB") 'self-insert-command)
+
+(provide 'setup-csv)
