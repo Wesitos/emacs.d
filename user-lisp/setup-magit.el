e4566aedc8847364647b37ecba770db6401ab2e8
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-28 04:22:40 -0500

    Agregado setup-magit

---
+++ user-lisp/setup-magit.el
@@ -0,0 +1,5 @@
+
+;; Hace que Magit pueda modificar los buffers al hacer cambios en el repo
+(setq magit-auto-revert-mode t)
+;; Para que no nos muestre un mensaje al inicio
+(setq magit-last-seen-setup-instructions "1.4.0")
