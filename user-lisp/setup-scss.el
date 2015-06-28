1b687d4b411ee234d2315f878a531adf9e676da8
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-28 03:21:57 -0500

    Agregado rainbow-mode para sass

--- user-lisp/setup-scss.el
+++ user-lisp/setup-scss.el
@@ -1,3 +1,10 @@
 ;; Evita que el sass se compile al grabar
 (setq scss-compile-at-save nil)
+
+(defun configure-auto-complete-for-scss ()
+  (add-to-list 'ac-sources 'ac-source-css-property))
+(add-hook 'scss-mode-hook 'configure-auto-complete-for-scss)
+(add-to-list 'ac-modes 'scss-mode)
+
+(add-hook 'css-mode-hook 'rainbow-mode)
 (provide 'setup-scss)
